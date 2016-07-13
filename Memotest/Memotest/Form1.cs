d59aafc5573c3904d5a7a1ce87292d23802626a6
soljudke <soljudke@outlook.com>
2016-07-13 17:23:04 -0300

    chau

---
+++ Memotest/Memotest/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memotest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            MaximizeBox = false;
+            //this.BackgroundImage = System.Drawing.Image.FromFile(Configuracion.RootFolder + "IMGS/FondoArmando2.jpg");
+            this.Location = new Point(100, 100);
+        }
+        PictureBox[] pictures;
+        int counter;
+        Tarjetas tarje = new Tarjetas();
+        List<Tarjetas> listTarjetas = new List<Tarjetas>();
+        int x;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Inicio();
+        }
+        private void Inicio()
+        {
+            counter = 60;
+            x = 250;
+            timer1.Interval = 1000;
+            timer1.Start();
+            lblTiempo.Text = counter.ToString();
+            lblTiempo.ForeColor = Color.Black;
+            listTarjetas = tarje.Traemelos();
+            pictures = new PictureBox[listTarjetas.Count()];
+            for (int i = 0; i < listTarjetas.Count(); i++)
+            {
+                pictures[i] = new PictureBox();
+                pictures[i].Image = System.Drawing.Image.FromFile(Configuracion.RootFolder + listTarjetas[i].foto);
+                pictures[i].AutoSize = true;
+                pictures[i].BackColor = Color.Transparent;
+                pictures[i].Location = new Point(x, 341);
+                x = x + 80;
+                this.Controls.Add(pictures[i]);
+                
+
+               // guiones[i].Name = i + splitSilaba[i].ToString();
+                
+                
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter == 0)
+            {
+                timer1.Stop();
+                CustomMessageForm mimsg = new CustomMessageForm("Perdiste");
+                DialogResult result = mimsg.ShowDialog();
+                if (result == DialogResult.Yes)
+                {
+                    Inicio();
+                }
+                else if (result == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+            if (counter == 15)
+                lblTiempo.ForeColor = Color.Red;
+            lblTiempo.Text = counter.ToString();
+        }
+    }
+}
---
+++ Memotest/Memotest/Tarjetas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+namespace Memotest
+{
+    class Tarjetas
+    {
+        public string foto { get; set; }
+        public int pareja{ get; set; }
+        public int nivel{ get; set; }
+        private OleDbConnection Conn;
+        public void AbrirConexion()
+        {
+            Conn = new OleDbConnection(Configuracion.ConnectionString);
+            Conn.Open();
+        }
+        public List<Tarjetas> Traemelos()
+        {
+            AbrirConexion();
+            OleDbCommand Consulta = Conn.CreateCommand();
+            Consulta.CommandType = CommandType.StoredProcedure;
+            Consulta.CommandText = "Traeme_Tarjetas";
+            OleDbDataReader traido = Consulta.ExecuteReader();
+            List<Tarjetas> lista = new List<Tarjetas>();
+            while (traido.Read())
+            {
+                Tarjetas tarje = new Tarjetas();
+                tarje.pareja= Convert.ToInt32(traido["IdJugador"]);
+                tarje.foto = traido["Nombre"].ToString();
+                tarje.nivel = Convert.ToInt32(traido["Nivel"]);
+                lista.Add(tarje);
+            }
+            Conn.Close();
+            return lista;
+        }
+    }
+}
